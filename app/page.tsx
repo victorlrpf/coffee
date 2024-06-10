44e32fee9f03ab4ec13240c81a13cee67430f16b
victor <victorlrpf@gmail.com>
2024-06-10 15:34:38 -0300

    feat: add config initials page home

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,17 @@
+import Carrosel from "./_components/carrosel";
+import HeaderPrincipal from "./_components/header";
+import Search from "./_components/search";
+
 export default function Home() {
   return (
     <>
-      <h2>THE COFFE</h2>
+    <header className="pt-6 px-5">
+      <HeaderPrincipal/>
+      <Search />
+    </header>
+    <main className="pt-6 px-5">
+      <Carrosel/>
+    </main>
     </>
   );
 }
