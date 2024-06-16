a332d048c1a34110cfe0f6a9570d608fd9217cd1
victor <victorlrpf@gmail.com>
2024-06-15 22:25:01 -0300

    feat: add page initial

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,10 @@
+import Link from "next/link";
 import Carrosel from "./_components/carrosel";
 import HeaderPrincipal from "./_components/header";
+import ProductList from "./_components/product-list";
 import Search from "./_components/search";
+import { ChevronRightIcon } from "lucide-react";
+import { Button } from "./_components/ui/button";
 
 export default function Home() {
   return (
@@ -9,8 +13,62 @@ export default function Home() {
       <HeaderPrincipal/>
       <Search />
     </header>
-    <main className="pt-6 px-5">
-      <Carrosel/>
+    <main className="pt-6 ">
+      <div className="px-5">
+        <Carrosel/>
+      </div>
+      
+      <div className="pt-6">
+        <div className="flex justify-between px-5">
+          <h2 className="text-lg font-semibold">Bebidas</h2>
+          <Button
+          variant="ghost"
+          className="h-fit p-0 text-primary hover:bg-transparent"
+          asChild
+          >
+            <Link href="/restaurants/recommended">
+              Ver todos
+              <ChevronRightIcon size={16} />
+            </Link>          
+          </Button>
+        </div>
+        <ProductList />
+      </div>
+      
+      <div className="pt-6">
+      <div className="flex justify-between px-5">
+        <h2 className="text-lg font-semibold">Salgados</h2>
+        <Button
+          variant="ghost"
+          className="h-fit p-0 text-primary hover:bg-transparent"
+          asChild
+          >
+            <Link href="/restaurants/recommended">
+              Ver todos
+              <ChevronRightIcon size={16} />
+            </Link>          
+          </Button>
+      </div>
+        <ProductList />
+      </div>
+      
+      <div className="pt-6">
+        <div className="flex justify-between px-5">
+          <h2 className="text-lg font-semibold">Doces</h2>
+          <Button
+            variant="ghost"
+            className="h-fit p-0 text-primary hover:bg-transparent"
+            asChild
+            >
+              <Link href="/restaurants/recommended">
+                Ver todos
+                <ChevronRightIcon size={16} />
+              </Link>          
+            </Button>
+        </div>
+        <ProductList />
+      </div>
+      
     </main>
     </>
   );
